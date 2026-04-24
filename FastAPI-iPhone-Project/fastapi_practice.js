2a30f2368f3fcdc7f48c10aa965f130c7fc6d334
Fossilest2 <rui@Louis.local>
2026-04-24 21:36:57 +0900

    feat: implement initial frontend integration with fetch API

--- FastAPI-iPhone-Project/fastapi_practice.js
+++ FastAPI-iPhone-Project/fastapi_practice.js
@@ -7,7 +7,18 @@ function setup()
 
 function draw()
 {
-  
   fill(0);
   print("This is practice frontend for combining with FastAPI ");
 }
+
+const get_iphoneData = async (userID) =>{
+    const response = await fetch ("http://127.0.0.1:8000/iphone/"+ userID)
+
+    if (response.ok){
+        const data = await response.json()
+        console.log(data.userID);
+    }else{
+        console.log ("The data is not found")
+    }
+}
+
