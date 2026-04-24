c044be50e1227ae4e209decda6f7ef785f911fb6
Fossilest2 <rui@Louis.local>
2026-04-24 21:35:10 +0900

    feat: migrate database to PostgreSQL and containerize with Docker Compose

--- FastAPI-iPhone-Project/database.py
+++ FastAPI-iPhone-Project/database.py
@@ -1,42 +1,70 @@
-import sqlite3
+# import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+SQLALCHEMY_DATABASE_URL = "postgresql://rui:password123@localhost:5432/iphone_management"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 def get_connection():
+    return psycopg2.connect(host = "localhost",
+                            database = "iphone_management",
+                            user = "rui",
+                            password = "password123",
+                            port = 5432,
+                            cursor_factory = RealDictCursor)
 
-    connect = sqlite3.connect("iphone.db")
-    connect.row_factory = sqlite3.Row 
-    return connect
 
-def init_database():
-    connect = get_connection()
-    cursor = connect.cursor()
+# def init_database():
+#     connect = get_connection()
+#     cursor = connect.cursor()
 
-    cursor.execute('''
-               CREATE TABLE IF NOT EXISTS iphones(
-                  id INTEGER PRIMARY KEY AUTOINCREMENT,
-                  version REAL,
-                  serialNumber INTEGER,
-                  userID INTEGER,
-                  userName TEXT
-                  phoneNumber INTEGER
-                  birthDay INTEGER
-                 )
-               ''')
-    cursor.execute('''
-                 CREATE TABLE IF NOT EXISTS deleted_iphones(
-                   id INTEGER PRIMARY KEY AUTOINCREMENT
-                   userID INTEGER
-                   userName TEXT)
-                 ''')
+#     cursor.execute('''
+#                CREATE TABLE IF NOT EXISTS iphones(
+#                   id SERIAL PRIMARY KEY,
+#                   version TEXT,
+#                   serialNumber TEXT,
+#                   userID INTEGER UNIQUE,
+#                   userName TEXT,
+#                   phoneNumber TEXT,
+#                   birthDay TEXT,
+#                  );
+#                ''')
+#     cursor.execute('''
+#                  CREATE TABLE IF NOT EXISTS deleted_iphones(
+#                    id SERIAL PRIMARY KEY,
+#                    userID INTEGER UNIQUE,
+#                    userName TEXT
+#                    );
+#                  ''')
     
-    cursor.execute('''
-                  CREATE TABLE IF NOT EXISTS workers(
-                   id INTEGER PRIMARY KEY AYTOINCREMENT,
-                   workerID: INTERGER
-                   country: TEXT
-                   age: INTEGER
-                   salary: str)
-                  ''')
+#     cursor.execute('''
+#                   CREATE TABLE IF NOT EXISTS workers(
+#                    id SERIAL PRIMARY KEY,
+#                    workerID: TEXT
+#                    country: TEXT
+#                    age: TEXT
+#                    salary: TEXT
+#                    );
+#                   ''')
+#     print("Successfully the postgreSQL is added for the database")
+#     connect.commit()
+#     connect.close()
+#     connect.close()
+#     connect.close()
     
-    connect.commit()
-    connect.close()
-    
+
--- FastAPI-iPhone-Project/models.py
+++ FastAPI-iPhone-Project/models.py
@@ -1,21 +1,29 @@
-from pydantic import BaseModel
+from database import Base
+from sqlalchemy import Column, Integer, String
 
-class Iphone(BaseModel):
-   version: int
-   serialNumber: int
-   userID: int
-   userName: str
-   phoneNumber: int
-   birthDay: int
-   
+class Iphone(Base):
+   __tablename__ = "iphones"
 
+   id = Column(Integer, primary_key=True, index=True)
+   version = Column(String)
+   serialNumber = Column(String)
+   userID = Column(String, unique=True, index=True)
+   userName = Column(String)
+   phoneNumber = Column(String)
+   birthDay = Column(String)
 
-class Deleted_iphone(BaseModel):
-    userID: int
-    userName: str
+class Deleted_iphone(Base):
+   __tablename__ = "deleted_iphones"
 
-class Workers(BaseModel):
-    workerID: int
-    country: str
-    age: int
-    salary: str
+   id = Column(Integer, primary_key=True, index=True)
+   userID = Column(Integer, unique=True, index=True)
+   serialNumber = Column(String, unique=True)
+
+class Workers(Base):
+   __tablename__ = "workers"
+
+   id = Column(Integer, primary_key=True, index=True)
+   workerID = Column(Integer, unique=True, index=True)
+   country = Column(String)
+   age = Column(Integer)
+   salary = Column(String)
